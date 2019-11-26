0575a2d77e1e8e419c558cbf8df7f30ac23f10d0
cimontgo <cimontgo@uncc.edu>
2019-11-26 10:25:47 -0500

    L1/Jun - Edits
    - Jun "Pulse" Enabled
    - Attempted Jun Relocation -- FAILED
    - More Wisps
    - Terrain Smoothing
    - Terrain Object Adjusts

--- Eiko/Assets/Scripts/Other/JunAI.cs
+++ Eiko/Assets/Scripts/Other/JunAI.cs
@@ -11,19 +11,40 @@ public class JunAI : MonoBehaviour
     public float rangeRadius = 15f;
     [SerializeField] private Transform JunPosition;
 
+    private Light lt;
+    float duration  = 2.0f;
+    float originalRange;
+//    private float moveDistance;
+    private Vector3 currentPos;
+//    private float speed = 3.0f;
+//    private float step;
+
     void Start()
     {
         navAgent = GetComponent<NavMeshAgent>();
+        lt = GameObject.Find("Jun").GetComponent<Light>();
+        originalRange = lt.range;
     }
 
     void Update()
     {
         transform.position = JunPosition.position;
+        
+        var amplitude = Mathf.PingPong(Time.time, duration);
+
+        // Transform from 0..duration to 0.5..1 range.
+        amplitude = amplitude / duration * 0.5f + 1f;
+
+        // Set light range.
+        lt.range = originalRange * amplitude;
 
         withinRangeColliders = Physics.OverlapSphere(transform.position, rangeRadius, rangeLayerMask);
         if (withinRangeColliders.Length > 0)
         {
             ChangeColorToRed();
+            //Relocation Failure
+            //RelocateJun();
+
         }
         else
         {
@@ -33,11 +54,31 @@ public class JunAI : MonoBehaviour
 
     void ChangeColorToRed()
     {
-        GameObject.Find("Jun").GetComponent<Light>().color = Color.green;
+        lt.color = Color.green;
     }
+    
+/*    void RelocateJun()
+    {
+        currentPos = GameObject.Find("Jun").transform.position;
+        step = speed * Time.deltaTime;
+        GameObject closestItem = null;
+        float closestItemDist = 0;
+        
+        foreach (var item in withinRangeColliders)
+        {
+            float itemDistanceFromJun = Vector3.Distance(item.gameObject.transform.position, currentPos);
+            if (itemDistanceFromJun < closestItemDist)
+            {
+                closestItemDist = itemDistanceFromJun;
+                closestItem = item.gameObject;
+            }
+        }
+        
+        transform.position = Vector3.MoveTowards(currentPos, closestItem.transform.position, step);
+    }*/
     void ChangeColorToWhite()
     {
-        GameObject.Find("Jun").GetComponent<Light>().color = Color.white;
+        lt.color = Color.white;
     }
 
 
